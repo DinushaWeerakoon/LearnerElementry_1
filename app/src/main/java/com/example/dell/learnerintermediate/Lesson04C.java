a85208b2f79145262a664431e698bb4996ffd983
Nirmani-N <nirmaninayanathara@gmail.com>
2015-09-08 15:15:19 +0530

    Lesson 04 - 1st activity completed

--- app/src/main/java/com/example/dell/learnerintermediate/Lesson04C.java
+++ app/src/main/java/com/example/dell/learnerintermediate/Lesson04C.java
@@ -1,5 +1,6 @@
 package com.example.dell.learnerintermediate;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,63 +9,72 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
-/**
- * Created by UCSC-PC on 6/4/2015.
- */
-public class Lesson04C extends Fragment {
 
-    TextView correct;
+public class Lesson04C extends Fragment {
 
 
     @Override
      public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view= inflater.inflate(R.layout.fr_less_04_c, container, false);
+
+        final View view = inflater.inflate(R.layout.fr_less_04_c, container, false);
+
+        final RadioButton rb1 = (RadioButton) view.findViewById(R.id.a);
+        final RadioButton rb2 = (RadioButton) view.findViewById(R.id.b);
+        final RadioButton rb3 = (RadioButton) view.findViewById(R.id.c);
+
+        View.OnClickListener first_radio_listener1 = new View.OnClickListener(){
+            public void onClick(View v) {
+
+                rb1.setTextColor(Color.GREEN);
+                rb2.setTextColor(Color.BLACK);
+                rb3.setTextColor(Color.BLACK);
+
+            }
+        };
+
+        View.OnClickListener first_radio_listener2 = new View.OnClickListener(){
+            public void onClick(View v) {
+
+                rb2.setTextColor(Color.RED);
+                rb1.setTextColor(Color.BLACK);
+                rb3.setTextColor(Color.BLACK);
 
 
-        correct = (TextView)view.findViewById(R.id.cor);
+            }
+        };
+
+        View.OnClickListener first_radio_listener3 = new View.OnClickListener(){
+            public void onClick(View v) {
+                rb3.setTextColor(Color.RED);
+                rb1.setTextColor(Color.BLACK);
+                rb2.setTextColor(Color.BLACK);
+
+            }
+        };
+
+        Button reset= (Button) view.findViewById(R.id.reset);
+        View.OnClickListener resetListener= new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rb1.setTextColor(Color.BLACK);
+                rb2.setTextColor(Color.BLACK);
+                rb3.setTextColor(Color.BLACK);
+                RadioGroup rg= (RadioGroup)view.findViewById(R.id.radio);
+                rg.clearCheck();
+
+            }
+        };
+
+        rb1.setOnClickListener(first_radio_listener1);
+        rb2.setOnClickListener(first_radio_listener2);
+        rb3.setOnClickListener(first_radio_listener3);
+        reset.setOnClickListener(resetListener);
+
         return view;
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-   public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.a:
-                if (checked)
-                    correct.setText("Correct :");
-                    break;
-
-
-            case R.id.b:
-                if (checked)
-                    correct.setText("Wrong :");
-                    break;
-
-            case R.id.c:
-                if (checked)
-                    correct.setText("Wrong :");
-                    break;
-        }
     }
 }
--- app/src/main/java/com/example/dell/learnerintermediate/Lesson21B.java
+++ app/src/main/java/com/example/dell/learnerintermediate/Lesson21B.java
@@ -1,11 +1,18 @@
 package com.example.dell.learnerintermediate;
 
+import android.content.ClipData;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 /**
  * Created by UCSC-PC on 6/4/2015.
@@ -15,7 +22,116 @@ public class Lesson21B extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fr_less_21_b, container, false);
+        final View view= inflater.inflate(R.layout.fr_less_21_b, container, false);
 
+        final Processor radio= new Processor();
+
+
+        TextView txt1= (TextView) view.findViewById(R.id.txt_1);
+        TextView txt2= (TextView) view.findViewById(R.id.txt_2);
+        TextView txt3= (TextView) view.findViewById(R.id.txt_3);
+        TextView txt4= (TextView) view.findViewById(R.id.txt_4);
+        TextView txt5= (TextView) view.findViewById(R.id.txt_5);
+        TextView txt6= (TextView) view.findViewById(R.id.txt_6);
+        TextView txt7= (TextView) view.findViewById(R.id.txt_7);
+
+        txt1.setTag("Choose an e-card company.There are many companies including 123 greetings,yahoo,hallmark etc.");
+        txt2.setTag("Decide on your e-card to match the situation.Preview the cards and select the best.");
+        txt3.setTag("Type and add a personal note to your card.Remember,it should be short.");
+        txt4.setTag("Select the option and preview your entire e-card.'Edit' the card if you have any mistakes.");
+        txt5.setTag("Include your email recipients.");
+        txt6.setTag("Select the option to receive a notice when the recipients open the cards.");
+        txt7.setTag("Send your e-card after sending up the date.");
+
+
+        View.OnLongClickListener ChoiceTouchLisner =new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                ClipData data = ClipData.newPlainText("", "");
+                DragShadow dragShadow = new DragShadow(v);
+
+                v.startDrag(data,dragShadow,v,0);
+                return false;
+            }
+        };
+
+        View.OnDragListener ChoiceDragLisner=new View.OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                int dragEvent = event.getAction();
+
+                TextView dragged = (TextView) event.getLocalState();
+
+                radio.processDragOrder(v, dragEvent, dragged);
+
+                return true;
+            }
+        };
+
+        txt1.setOnLongClickListener(ChoiceTouchLisner);
+        txt2.setOnLongClickListener(ChoiceTouchLisner);
+        txt3.setOnLongClickListener(ChoiceTouchLisner);
+        txt4.setOnLongClickListener(ChoiceTouchLisner);
+        txt5.setOnLongClickListener(ChoiceTouchLisner);
+        txt6.setOnLongClickListener(ChoiceTouchLisner);
+        txt7.setOnLongClickListener(ChoiceTouchLisner);
+
+        txt1.setOnDragListener(ChoiceDragLisner);
+        txt2.setOnDragListener(ChoiceDragLisner);
+        txt3.setOnDragListener(ChoiceDragLisner);
+        txt4.setOnDragListener(ChoiceDragLisner);
+        txt5.setOnDragListener(ChoiceDragLisner);
+        txt6.setOnDragListener(ChoiceDragLisner);
+        txt7.setOnDragListener(ChoiceDragLisner);
+
+        final RadioButton rb1= (RadioButton)view.findViewById(R.id.btn_1);
+        final RadioButton rb2= (RadioButton)view.findViewById(R.id.btn_2);
+        final RadioButton rb3= (RadioButton)view.findViewById(R.id.btn_3);
+        final RadioButton rb4= (RadioButton)view.findViewById(R.id.btn_4);
+        final RadioButton rb5= (RadioButton)view.findViewById(R.id.btn_5);
+        final RadioButton rb6= (RadioButton)view.findViewById(R.id.btn_6);
+
+        rb4.setTag("yes");
+
+        Button reset1= (Button) view.findViewById(R.id.reset_btn1);
+        Button reset2= (Button) view.findViewById(R.id.reset_btn);
+
+        View.OnClickListener radiolistner= new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rb1.setTextColor(Color.BLACK);
+                rb2.setTextColor(Color.BLACK);
+                rb3.setTextColor(Color.BLACK);
+                rb4.setTextColor(Color.BLACK);
+                rb5.setTextColor(Color.BLACK);
+                rb6.setTextColor(Color.BLACK);
+                radio.processRadioButton(v);
+            }
+        };
+
+
+        View.OnClickListener resetlisnr2= new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rb1.setTextColor(Color.BLACK);
+                rb2.setTextColor(Color.BLACK);
+                rb3.setTextColor(Color.BLACK);
+                rb4.setTextColor(Color.BLACK);
+                rb5.setTextColor(Color.BLACK);
+                rb6.setTextColor(Color.BLACK);
+
+                RadioGroup rg1= (RadioGroup)view.findViewById(R.id.grp);
+                rg1.clearCheck();
+
+            }
+        };
+        rb1.setOnClickListener(radiolistner);
+        rb2.setOnClickListener(radiolistner);
+        rb3.setOnClickListener(radiolistner);
+        rb4.setOnClickListener(radiolistner);
+        rb5.setOnClickListener(radiolistner);
+        rb6.setOnClickListener(radiolistner);
+        reset2.setOnClickListener(resetlisnr2);
+        return view;
 
     }}
